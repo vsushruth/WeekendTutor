3b49e291e8cf3545ad3f353f9ae8cce22eb18d4a
vsushruth <32487818+vsushruth@users.noreply.github.com>
2019-03-30 12:33:49 +0530

    Update Welcome.java

--- app/src/main/java/com/chetan/wt/Welcome.java
+++ app/src/main/java/com/chetan/wt/Welcome.java
@@ -8,8 +8,14 @@ import android.os.Bundle;
 import android.view.*;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Welcome extends AppCompatActivity {
     int flag=0;
+    public static int loginState = 0;
+    private String id;
+    private FirebaseAuth fa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,20 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         Intent intent = getIntent();
+
+        FirebaseUser cuser = fa.getCurrentUser();
+        id = cuser.getUid();
+
+        if(loginState == 1 && !id.equals(null))
+        {
+            Intent stu = new Intent(Welcome.this, CourseList.class);
+            startActivity(stu);
+        }
+        else if(loginState == 2 && id != null)
+        {
+            Intent tut = new Intent(Welcome.this, ListOfCourseTutor.class);
+            startActivity(tut);
+        }
     }
 
     public void login_tutor(View view){
@@ -33,18 +53,14 @@ public class Welcome extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            if(flag==1){
-                finishAffinity();
-            }
-            else {
-                Toast.makeText(this,"Press once again to exit",Toast.LENGTH_SHORT).show();
-                flag++;
-            }
+        if(flag==1){
+            finishAffinity();
+        }
+        else {
+            Toast.makeText(this,"Press once again to exit",Toast.LENGTH_SHORT).show();
+            flag++;
         }
     }
 
 }
+
