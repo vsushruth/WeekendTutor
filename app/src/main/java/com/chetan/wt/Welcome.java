733a75470f498c1792693ad3de660fdcacef5335
vsushruth <32487818+vsushruth@users.noreply.github.com>
2019-03-31 17:44:33 +0530

    Update Welcome.java

--- app/src/main/java/com/chetan/wt/Welcome.java
+++ app/src/main/java/com/chetan/wt/Welcome.java
@@ -1,6 +1,7 @@
 package com.chetan.wt;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,20 @@ import android.os.Bundle;
 import android.view.*;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Welcome extends AppCompatActivity {
-    int flag=0;
+    int flag = 0;
+    private String UserID = null;
+    private FirebaseAuth fa;
+    private FirebaseUser User;
+    private DatabaseReference refStud, refTut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +31,54 @@ public class Welcome extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
 
-        Intent intent = getIntent();
+        fa = FirebaseAuth.getInstance();
+        User = fa.getCurrentUser();
+
+        refStud = FirebaseDatabase.getInstance().getReference("Students");
+        refTut = FirebaseDatabase.getInstance().getReference("users");
+        if (User != null) {
+
+            UserID = User.getUid();
+            if(UserID != null)
+            {
+                refStud.child(UserID).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //UserClass = dataSnapshot.child("userClass").getValue().toString();
+                        if(dataSnapshot.exists())
+                        {
+                            Intent intent = new Intent(Welcome.this, CourseList.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                refTut.child(UserID).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()) {
+                            //UserClass = dataSnapshot.child("userClass").getValue().toString();
+                            Intent intent = new Intent(Welcome.this, CourseList.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+            }
+        }
     }
 
     public void login_tutor(View view){
@@ -33,17 +93,12 @@ public class Welcome extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            if(flag==1){
-                finishAffinity();
-            }
-            else {
-                Toast.makeText(this,"Press once again to exit",Toast.LENGTH_SHORT).show();
-                flag++;
-            }
+        if(flag==1){
+            finishAffinity();
+        }
+        else {
+            Toast.makeText(this,"Press once again to exit",Toast.LENGTH_SHORT).show();
+            flag++;
         }
     }
 
